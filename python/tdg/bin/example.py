Sign fixes: we require the attractive channel

python/tdg/bin/example.py
@@ -60,10 +60,10 @@ print(f"A linearized list of coordinates has length {len(spacetime.coordinates)}
 section("Lego Spheres")
 
 print("A LegoSphere just requires a radius and a Wilson coefficient.")
-contact = tdg.LegoSphere([0,0], 2)
+contact = tdg.LegoSphere([0,0], -2)
 print(f"This sphere {contact} has radius {contact.r} and coefficient {contact.c}.")
 print("The default coefficient is 1, and LegoSpheres may be multiplied by coefficients.")
-sphere = 1.5 * tdg.LegoSphere([0,1])
+sphere = 0.25 * tdg.LegoSphere([0,1])
 print(f"This sphere {sphere} has radius {sphere.r} and coefficient {sphere.c}.")
 print(f"The points on this LegoSphere are {sphere.points} from the center.")
 
@@ -86,9 +86,9 @@ else:
 
 try:
     print(f"The eigenvalues of V are\n{V.eigvals(lattice)}.")
-    print(f"All the eigenvalues of V on {lattice} are positive; this is amenable to our Hubbard-Stratonovich transformation.")
+    print(f"All the eigenvalues of V on {lattice} are negative; this is amenable to our attractive-channel Hubbard-Stratonovich transformation.")
 except:
-    print("Not all the eigenvalues of V on {lattice} are positive, so the formal Hubbard-Stratonovich transformation is invalid for this potential.")
+    print("Not all the eigenvalues of V on {lattice} are negative, so the formal Hubbard-Stratonovich transformation is invalid for this potential.")
 print(f"The spatial representation, inverse, and eigenvalues are cached, so it's cheap to reuse them.")
 print(f"Since the contact potential needs to be treated specially, the potential provides a way to get the C0 coefficient {V.C0=}")
 
@@ -97,7 +97,7 @@ section("Actions")
 
 print("An action collects all the information about what we're up to.")
 
-beta = torch.tensor(50)
+beta = torch.tensor(1)
 S = tdg.Action(spacetime, V, beta)
 A = spacetime.vector()
 print(f"The free action is {S(A)}.")

python/tdg/tdg/action.py
@@ -27,6 +27,8 @@ class Action:
         # Since we work with H-µN-hS this ADDS to the physical chemical potential.
         self.FermionMatrix = fermion(self.Spacetime, self.beta, mu=self.mu + potential.C0/2, h=self.h)
 
+        self.normalizing_offset = self.Spacetime.nt / 2 * torch.sum( torch.log(-2*torch.pi*self.dt * self.Potential.eigvals(self.Spacetime.Lattice)))
+
     def __str__(self):
         return f"Action(β={self.beta}, µ={self.mu}, h={self.h}, {self.Spacetime}, {self.Potential})"
 
@@ -34,10 +36,10 @@ class Action:
         return str(self)
 
     def __call__(self, A):
-        # S = 1/2 Σ(t) A(t) inverse(- ∆t V) A(t) - log det d
-        gauss = 0.5 * torch.einsum('txy,xyab,tab->', A, -self.Vinverse, A) / self.dt
+        # S = 1/2 Σ(t) A(t) inverse(- ∆t V) A(t) - log det d + nt/2 tr log(-2π ∆t V )
+        gauss = -0.5 * torch.einsum('txy,xyab,tab->', A, self.Vinverse.to(A.dtype), A) / self.dt
 
         fermionic = - self.FermionMatrix.logdet(A)
 
-        return gauss + fermionic
+        return gauss + fermionic + self.normalizing_offset
 

python/tdg/tdg/potential.py
@@ -29,8 +29,8 @@ class Potential:
         e = torch.linalg.eigvals(lattice.tensor_linearized(self.spatial(lattice)))
         if (e.imag != 0).all():
             raise TypeError(f"{self} yields imaginary eigenvalues.")
-        if (e.real <= 0).any():
-            raise ValueError(f"{self} yields negative eigenvalues.")
+        if (e.real >= 0).any():
+            raise ValueError(f"{self} yields positive eigenvalues; we require the attractive channel.")
         #return e.real ?
         return e
 