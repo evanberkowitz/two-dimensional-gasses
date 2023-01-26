808b29bfa730bc6bef7a935072af163dc344f05f
Evan Berkowitz <git@evanberkowitz.com>
2023-01-27 00:10:09 +0100

    setup.py is a stub; pyproject.toml has details

--- python/tdg/setup.py
+++ python/tdg/setup.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    setuptools.setup()#!/usr/bin/env python
 
-    setup(
-            name='tdg',
-            description='Auxiliary field quantum monte carlo for 2D gasses',
-
-            version='0.0.0',
-            license='GPL-3',
-
-            author='Evan Berkowitz',
-            author_email='tdg@evanberkowitz.com',
-            url = "https://github.com/evanberkowitz/two-dimensional-gasses/",
-
-            )
