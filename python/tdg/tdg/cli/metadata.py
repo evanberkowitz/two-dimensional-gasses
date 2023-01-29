1d8ae1e3bedfdfa4fc5c847aeb4c3e6f02fc3d93
Evan Berkowitz <git@evanberkowitz.com>
2023-01-29 20:25:20 +0100

    Introduce metadata, log, default ArgumentParser

---
+++ python/tdg/tdg/cli/metadata.py
@@ -0,0 +1,28 @@
+import tdg.meta
+import argparse
+
+def print_and_exit(description, data):
+
+    class anonymous(argparse.Action):
+
+        def __call__(self, parser, namespace, values, option_string):
+            print(data)
+            exit()
+
+    return {
+            'help':   f'Exit after printing {description}.',
+            'action': anonymous,
+            'nargs':  0,
+            }
+
+def defaults():
+    meta_arguments = argparse.ArgumentParser(add_help=False)
+    meta_arguments.add_argument('--version', **print_and_exit("the version", tdg.meta.version))
+
+    return meta_arguments
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(parents=[defaults(), ]) 
+    args = parser.parse_args()
+
