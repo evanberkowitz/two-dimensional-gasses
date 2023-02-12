e369f9c006d9f19bd84e2d6e3b1889fab0f44535
Evan Berkowitz <git@evanberkowitz.com>
2023-02-12 23:48:45 +0100

    Document the observable.py back-stage story

--- python/tdg/tdg/observable/observable.py
+++ python/tdg/tdg/observable/observable.py
@@ -4,6 +4,16 @@ import tdg.ensemble
 import logging
 logger = logging.getLogger(__name__)
 
+# To allow users to add observables to GrandCanonical requires some design considerations.
+# 
+#   https://github.com/evanberkowitz/two-dimensional-gasses/issues/12
+# 
+# The strategy is to use the descriptor protocol
+# 
+#   https://docs.python.org/3/howto/descriptor.html
+# 
+# to hook observables in to the GrandCanonical class.
+
 ####
 #### Observable interface
 ####
@@ -11,6 +21,9 @@ logger = logging.getLogger(__name__)
 class Observable:
 
     def __init_subclass__(cls, name=''):
+        # This registers every subclass that inherits from Observable.
+        # Upon registration, GrandCanonical gets an attribute with the appropriate name.
+
         if name == '':
             cls.name = cls.__name__
         else:
@@ -22,15 +35,28 @@ class Observable:
         self.name  = name
 
     def __get__(self, obj, objtype=None):
+        # The __get__ method is the workhorse of the Descriptor protocol.
+
         # Cache:
         if self.name in obj.__dict__:
+            # What's nice about this is that the cache is in the object's dictionary itself,
+            # rather than associated with the observable class.  This avoids the issue of a
+            # class level cache discussed in https://github.com/evanberkowitz/two-dimensional-gasses/issues/12
+            # in that there's no extra reference to the object at all with this strategy.
+            # So, when it goes out of scope with no reference, it will be deleted.
             return obj.__dict__[self.name]
 
         if objtype is tdg.ensemble.GrandCanonical:
+            # Just call the measurement and cache the result.
             result = self.measure(obj)
             obj.__dict__[self.name] = result
             return result
 
+        # It may be possible to further generalize and implement the canonical projections
+        # or data analysis like binning and bootstrapping by detecting the class here and
+        # giving a different implementation.
+        #
+        # But for now,
         raise NotImplemented()
 
     def __set__(self, obj, value):
@@ -41,6 +67,7 @@ class Observable:
 ####
 
 class CallableObservable:
+    # The primary difference between this and an Observable is the __get__
 
     def __init_subclass__(cls, name=''):
         if name == '':
@@ -54,6 +81,15 @@ class CallableObservable:
         self.name = name
 
     def __get__(self, obj, objtype=None):
+        # Since we know the measurement requires arguments, not just the ensemble object itself,
+        # It's not so easy (though presumably possible?) to design an object-level cache that
+        # avoids keeping ensembles alive accidentally.
+        # 
+        # However, this is not SUCH a big deal.  See, for instance, the implementation of
+        # 
+        #   tdg.observable.number.n(ensemble, method)
+        # 
+        # which is actually just a dispatch to different parameter-free observables.
         if objtype is tdg.ensemble.GrandCanonical:
             def curried(*args, **kwargs):
                 return self.measure(obj, *args, **kwargs)
@@ -65,17 +101,31 @@ class CallableObservable:
 ####
 
 def observable(func):
+    # Now we are ready to decorate a function and turn it into a Descriptor.
+    # The primary decision is:
+    # 
+    #   Does this function need to be a CallableObservable?
+    #   Or can it just be an Observable?
+    # 
+    # We can decide that by counting its arguments.
     sig = signature(func)
     parameters = len(sig.parameters)
 
     if parameters == 0:
-        raise
+        raise TypeError('An @observable must take at least the ensemble as an argument')
+
+    # We assume functions of one parameter depend on the ensemble, rather than some
+    # other single argument.  Otherwise in what sense it is an observable?
+
     elif parameters == 1:
+
         class anonymous(Observable, name=func.__name__):
             
             def measure(self, ensemble):
                 return func(ensemble)
+
     elif parameters > 1:
+
         class anonymous(CallableObservable, name=func.__name__):
 
             def measure(self, ensemble, *args, **kwargs):
