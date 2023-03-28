evanberkowitz/two-dimensional-gasses

commit d6d6cd4d4744b2b2055bf11199ac5fdce9ad603d

Name the propagator G and implement

python/tdg/tdg/observable/UU.py
@@ -23,21 +23,29 @@ def _UU(ensemble):
     # A matrix for each configuration.
     return functorch.vmap(ensemble.Action.FermionMatrix.UU)(ensemble.configurations)
 
-@observable
-def _UUPlusOne(ensemble):
-    # A matrix for each configuration.
-    return torch.eye(2*ensemble.Action.Spacetime.Lattice.sites) + ensemble._UU
-
-@observable
 def _detUUPlusOne(ensemble):
-    return torch.det(ensemble._UUPlusOne)
-
-@observable
-def _UUPlusOneInverse(ensemble):
-    # A matrix for each configuration.
-    return torch.linalg.inv(ensemble._UUPlusOne)
+    # One per configuration.
+    UUPlusOne = torch.eye(2*ensemble.Action.Spacetime.Lattice.sites) + ensemble._UU
+    return torch.det(UUPlusOne)
 
 @observable
 def _UUPlusOneInverseUU(ensemble):
     # A matrix for each configuration.
+    UUPlusOne = torch.eye(2*ensemble.Action.Spacetime.Lattice.sites) + ensemble._UU
+
+    # TODO: do this via a solve rather than a true inverse?
+    inverse = torch.linalg.inv(ensemble._UUPlusOne)
     return torch.matmul(ensemble._UUPlusOneInverse,ensemble._UU)
+
+@observable
+def G(ensemble):
+    r'''
+    The equal-time propagator that is the contraction of :math:`\psi^\dagger_{a\sigma} \psi_{b\tau}`
+    where :math:`a` and :math:`b` are sites and :math:`\sigma` and :math:`\tau` are spins.
+
+    .. math ::
+       \mathcal{G} = [ \mathbb{U} (\mathbb{1} + \mathbb{U})^{-1} ]_{ab}^{\sigma\tau}
+    
+    A five-axis tensor: configurations slowest, then :math:`a`, :math:`b`, :math:`\sigma`, and :math:`\tau`.
+    '''
+    return ensemble._matrix_to_tensor(ensemble._UUPlusOneInverseUU)

macros.tex
@@ -12,6 +12,7 @@
 \newcommand{\dd}{\ensuremath{\mathbbm{d}}\xspace}
 \newcommand{\DD}{\ensuremath{\mathbb{D}}\xspace}
 \newcommand{\FF}{\ensuremath{\mathbb{F}}\xspace}
+\newcommand{\G}{\ensuremath{\mathcal{G}\xspace}}
 \newcommand{\Ham}{\ensuremath{\mathcal{H}}\xspace}
 \newcommand{\MM}{\ensuremath{\mathbb{M}}\xspace}
 \newcommand{\PP}{\ensuremath{\mathbb{P}}\xspace}

section/partition-function.tex
@@ -969,12 +969,12 @@ Now we replace $Z$ with the computational generating function,
 \end{align}
 exactly as before \eqref{fermionic particle number}!
 
-The generic lesson is that we may perform Wick contractions on normal-ordered operators, replacing
+The generic lesson is that we may perform Wick contractions on normal-ordered operators, replacing the contraction with the equal-time \emph{propagator} $\G$,
 \begin{align}
 	\contraction{}{\tilde{\psi}\adjoint_{x\sigma}}{}{\tilde{\psi}_{y\tau}}
 	\tilde{\psi}\adjoint_{x\sigma}\tilde{\psi}_{y\tau}
 	\goesto
-	\left[\UU (\one+\UU)\inverse\right]_{xy}^{\sigma\tau}
+	\G^{\sigma\tau}_{xy} = \left[\UU (\one+\UU)\inverse\right]_{xy}^{\sigma\tau}
 \end{align}
 as long as we track the number of minus signs that come from `crossing' in the contraction.
 For an example, see the double occupancy \Secref{double occupancy}.

commit d36e3ae92741af2a0c2662c63c28caf6b723c6d4

Regression fix: Canonical projection succeeds
sanity-checks/canonical-double-occupancy.py was failing, but there was
not a numerical issue; evaluating _canonical_weights failed.

The issue was that as part of cleaning up the UU observables, the fermion
determinant was not made @observable.  This was a bug introduced in commit
d6d6cd4d4744b2b2055bf11199ac5fdce9ad603d
and somehow I hadn't run the since.

This returns the @observable decorator to _detUUPlusOne, which is needed
for the _canonical_weights.  As soon as it was restored the sanity check
passed.

python/tdg/tdg/observable/UU.py
@@ -23,6 +23,7 @@ def _UU(ensemble):
     # A matrix for each configuration.
     return functorch.vmap(ensemble.Action.FermionMatrix.UU)(ensemble.configurations)
 
+@observable
 def _detUUPlusOne(ensemble):
     # One per configuration.
     UUPlusOne = torch.eye(2*ensemble.Action.Spacetime.Lattice.sites) + ensemble._UU