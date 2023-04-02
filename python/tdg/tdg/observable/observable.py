4febe5d893091a722f37f06bba677385c542968a
Evan Berkowitz <git@evanberkowitz.com>
2023-04-02 23:44:30 +0200

    Get rid of callable observables.

--- python/tdg/tdg/observable/observable.py
+++ python/tdg/tdg/observable/observable.py
@@ -66,43 +66,6 @@ class Observable:
         setattr(obj, self.name, value)
 
 ####
-#### Callable observable interface
-####
-
-class CallableObservable:
-    # The primary difference between this and an Observable is the __get__
-
-    def __init_subclass__(cls, name=''):
-        if name == '':
-            cls.name = cls.__name__
-        else:
-            cls.name = name
-        if cls.name[0] == '_':
-            logger.debug(f'Observable registered: {cls.name}')
-        else:
-            logger.info(f'Observable registered: {cls.name}')
-        setattr(tdg.ensemble.GrandCanonical, cls.name, cls())
-
-    def __set_name__(self, owner, name):
-        self.name = name
-
-    def __get__(self, obj, objtype=None):
-        # Since we know the measurement requires arguments, not just the ensemble object itself,
-        # It's not so easy (though presumably possible?) to design an object-level cache that
-        # avoids keeping ensembles alive accidentally.
-        # 
-        # However, this is not SUCH a big deal.  See, for instance, the implementation of
-        # 
-        #   tdg.observable.number.n(ensemble, method)
-        # 
-        # which is actually just a dispatch to different parameter-free observables.
-        if objtype is tdg.ensemble.GrandCanonical:
-            def curried(*args, **kwargs):
-                return self.measure(obj, *args, **kwargs)
-            return curried
-        raise NotImplemented()
-
-####
 #### The Decorator
 ####
 
@@ -117,25 +80,13 @@ def observable(func):
     sig = signature(func)
     parameters = len(sig.parameters)
 
-    if parameters == 0:
-        raise TypeError('An @observable must take at least the ensemble as an argument')
+    if parameters != 1:
+        raise TypeError(f'An @observable must take exactly one argument (the ensemble), not {parameters}.')
 
-    # We assume functions of one parameter depend on the ensemble, rather than some
-    # other single argument.  Otherwise in what sense it is an observable?
-
-    elif parameters == 1:
-
-        class anonymous(Observable, name=func.__name__):
-            
-            def measure(self, ensemble):
-                return func(ensemble)
-
-    elif parameters > 1:
-
-        class anonymous(CallableObservable, name=func.__name__):
-
-            def measure(self, ensemble, *args, **kwargs):
-                return func(ensemble, *args, **kwargs)
+    class anonymous(Observable, name=func.__name__):
+        
+        def measure(self, ensemble):
+            return func(ensemble)
 
     return func # This is a hack to get sphinx to document observables sensibly.
 
