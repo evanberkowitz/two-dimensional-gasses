2c3108be4f268c1306965035778584496d2526b7
Evan Berkowitz <git@evanberkowitz.com>
2023-05-19 22:01:55 +0200

    Autotuner!

--- python/tdg/tdg/HMC.py
+++ python/tdg/tdg/HMC.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 import torch
+import pandas as pd
+import numpy as np
+import scipy
 from tdg import _no_op
 from tdg.h5 import H5able
+import tdg.ensemble
 from collections import deque
 
+import warnings
 import logging
 logger = logging.getLogger(__name__)
 
@@ -315,4 +320,302 @@ class Omelyan(H5able):
         Forwards to ``integrate``.
         '''
         return self.integrate(x_i, p_i)
+
+class Autotuner:
+    r'''
+    An Autotuner seeks for a molecular dynamics discretization, targeting a given acceptance rate.
+    Based on the simple ideas explained in Reference :cite:`Krieg:2018pqh`.
+
+    Parameters
+    ----------
+        HMC_Hamiltonian: tdg.HMC.Hamiltonian
+            The HMC_Hamiltonian is used for molecular dynamics integration and accept/reject.
+            In principle these can be different, but that is not implemented here.
+
+        integrator: a molecular dynamics integrator
+            Could be tdg.HMC.LeapFrog, tdg.HMC.Omelyan, or another integrator
+            which just has one molecular dynamics discretization scale and is constructed via
+            ``integrator(HMC_Hamiltonian, md_steps, md_time)`` where a good value for md_steps is what we seek.
+
+        cfgs_per_estimate: int
+            How many trajectories are needed get a good estimate of the acceptance rate?
+            Reference :cite:`Krieg:2018pqh` found that (for the Hubbard model on the honeycomb lattice, but for very large lattices near the continuum limit)
+            as few as 5 could produce a good estimate, while they never needed more than about 30.
+    '''
+
+    def __init__(self, HMC_Hamiltonian, integrator, *, cfgs_per_estimate=5, _bootstrap_resamples=100):
+
+        self.H = HMC_Hamiltonian
+        self.I = integrator
+        self.cfgs_per_estimate = cfgs_per_estimate
+
+        self._bootstraps = _bootstrap_resamples
+
+        self.measurements = pd.DataFrame(data=None, columns=('md_steps', 'target', 'dH', 'acceptance', '<acc>', 'd<acc>', 'fit', 'prediction', 'N_bosonic'))
+        self.summary = pd.DataFrame(data=None, columns=('md_steps', '<acc>', 'd<acc>'))
+        self._fit = None
+
+    def target(self, target_acceptance=0.75, start='hot', md_time=1., starting_md_steps=20, min_md_steps=1, progress=_no_op):
+        '''
+        Runs the autotuning process to achieve a target acceptance rate.
+
+        The autotuning process seeks to adjust the number of molecular dynamics steps per trajectory to achieve a
+        specific acceptance rate (target_acceptance). It iteratively computes trajectories with different discretizations until
+        the acceptance rate meets the target. The autotuning stops when either the target acceptance rate is achieved with some confidence
+        or the discretization cannot be shrunk further without falling below the target.
+
+        Parameters
+        ----------
+            target_acceptance : float
+                The desired acceptance rate to achieve.
+                If the acceptance rate depends very sharply on the number of molecular dynamics steps, there may be no choice which
+                achieves the target.  In that case a finer molecular dynamics integration will be picked.
+            start : str, optional
+                The starting condition for the autotuning process.  Must be understood by :meth:`~.GrandCanonical.generate`.
+            md_time : float
+                The time duration for each molecular dynamics trajectory.
+            starting_md_steps : int
+                The initial number of molecular dynamics steps per trajectory.
+            min_md_steps : int
+                The minimum number of molecular dynamics steps allowed. Default is 1.
+            progress : callable
+                A callback function like `tqdm`_ that can be used to track HMC evolution.
+
+        Returns
+        -------
+            integrator : molecular dynamics integrator
+                An integrator object constructed with the molecular dynamics discretization tuned to the target acceptance.
+            configuration: torch.tensor
+                The final configuration resulting from all the trajectories accepted during the tuning process, which can be used in subsequent HMC.
+
+        .. _tqdm: https://pypi.org/project/tqdm/
+        '''
+        self.md_steps = starting_md_steps
+        self.md_steps_min = min_md_steps
+
+        logger.info(f'Tuning to {target_acceptance=} starting with {self.md_steps} MD steps.')
+
+        while (
+            (not (self.measurements['md_steps']==self.md_steps).any()) or
+            ((self.summary['<acc>']-self.summary['d<acc>']).loc[self.summary['md_steps']==self.md_steps]
+             < target_acceptance).all()
+        ):
+
+            logger.info(f'Computing trajectories with {self.md_steps} molecular dynamics steps per trajectory...')
+            start = self._step(self.md_steps, start=start, md_time=md_time, progress=progress)
+            self.measurements['target'].iloc[-1] = target_acceptance
+            self.md_steps = self._predict(target_acceptance)
+
+        return self.I(self.H, self.md_steps, md_time=md_time), self._latest_ensemble.configurations[-1]
+
+    def plot_history(self, ax, **kwargs):
+        '''
+        This method plots the acceptance probabilities over the course of the autotuning process. The acceptance probabilities
+        are shown for each trajectory, with the x-axis representing the trajectory index and the y-axis representing the
+        acceptance probability. The plot allows visualizing the change in acceptance probabilities over molecular dynamics time.
+
+        Parameters
+        ----------
+        ax : matplotlib.axes.Axes
+            The axes on which to plot the history.
+        **kwargs
+            Additional keyword arguments to customize the plot appearance.  Applied to the trace for every tuning step.
+        '''
+
+        ax.set_ylim([-0.05, 1.1])
+
+        step = 0
+        for key, series in self.measurements.iterrows():
+            y = series['acc']
+            n = len(y)
+            x = step + np.arange(n)
+            ax.plot(x, y, **kwargs)
+            ax.text(x.mean(), 1.05, f'{series["md_steps"]}', ha='center', va='center')
+            step += n
+
+        ax.set_xlabel('Trajectory')
+        ax.set_ylabel('Acceptance probability')
+
+    def plot_models(self, ax):
+        '''
+        Plots the evolution of acceptance probability models with autotuning.
+        Also shows, as points with error bars, the final estimated acceptance rates.
+
+        Parameters
+        ----------
+        ax : matplotlib.axes.Axes
+            The axes on which to plot the acceptance probability models.
+
+        '''
+
+
+        x=np.linspace(0,1.1*max(self.summary['md_steps']),1000)
+
+        # An alternate way of showing the target acceptance:
+        #
+        # if (self.measurements['target'] == self.measurements['target'][0]).all():
+        #     ax.axhline(self.measurements['target'][0], zorder=-1, color='black')
+
+        # This shows how to go from the prediction to the next md_step evaluation.
+        for (k1, s1), (k2, s2) in zip(self.measurements.iloc[:-1].iterrows(), self.measurements.iloc[1:].iterrows()):
+            trace = ax.plot(x, scipy.stats.gamma.cdf(x, *s1['fit']), label=s1['md_steps'])
+            color = trace[0].get_color()
+            ax.plot(
+                (s1['prediction'],np.ceil(s1['prediction']),np.ceil(s1['prediction'])),
+                (s1['target'],s1['target'], s2['<acc>']),
+                color=color, linestyle=':',
+                zorder=100+k1
+            )
+
+            ax.errorbar((s1['md_steps'],), (s1['<acc>'],), yerr=(s1['d<acc>'],), color=color)
+            ax.plot((0.2+s1['md_steps'],), (s1['<acc>'],), color=color, marker='<')
+
+        # but because it's two offset copies zipped, we still need to visualize the final measurement.
+        s1 = self.measurements.iloc[-1]
+        trace = ax.plot(x, scipy.stats.gamma.cdf(x, *s1['fit']), label=s1['md_steps'])
+        color = trace[0].get_color()
+        ax.errorbar((s1['md_steps'],), (s1['<acc>'],), yerr=(s1['d<acc>'],), color=color)
+        ax.plot((0.2+s1['md_steps'],), (s1['<acc>'],), color=color, marker='<')
+
+        ax.set_xlabel('Molecular dynamics steps')
+        ax.set_ylabel('Acceptance probability')
+
+    def _step(self, md_steps, *, md_time=1, start, progress=_no_op):
+        '''
+        This method performs a single step of the autotuning process with a specific number of molecular dynamics steps (md_steps).
+        It initializes the molecular dynamics integrator and uses it for HMC evolution.  It evaluates acceptance rates,
+        and updates the internal measurements and summary.
+
+        Parameters
+        ----------
+        md_steps : int
+            The number of molecular dynamics steps per trajectory for the current step.
+        md_time : int, optional
+            The time duration for each molecular dynamics trajectory.
+        start : str
+            The starting condition for the molecular dynamics trajectories.  Must be understood by :func:`~.GrandCanonical.generate`.
+        progress : callable, optional
+            A callback function like tqdm that can be used to track HMC evolution.
+
+        Returns
+        -------
+        torch.Tensor
+            The last configuration from the ensemble after running the trajectory.
+
+        '''
+        integrator = self.I(self.H, md_steps, md_time)
+        hmc = MarkovChain(self.H, integrator)
+
+        self._latest_ensemble = tdg.ensemble.GrandCanonical(self.H.V).generate(self.cfgs_per_estimate, hmc, start=start, progress=progress)
+
+        this_step = pd.Series({
+            'md_steps':   md_steps,
+            'dH':         torch.tensor(hmc.dH),
+            'acceptance': hmc.accepted / hmc.steps,
+            # We calculate N_bosonic specifically because it's already simple sums.
+            # Anything else requires the propagator, which we don't want in this loop.
+            'N_bosonic':  self._latest_ensemble.N_bosonic.cpu().clone().detach().numpy().real,
+        })
+        this_step['acc'] = torch.exp(-this_step['dH'].real).clip(max=1).numpy()
+        this_step['<acc>'] = this_step['acc'].mean()
+
+        self.measurements = pd.concat((self.measurements, this_step.to_frame().transpose()), axis=0, ignore_index=True)
+
+        return self._latest_ensemble.configurations[-1]
+
+
+    def _predict(self, target_acceptance=0.75, continuum=1000, maxfev=10000, uncertainty_floor=0.02):
+        '''
+        Given all previous HMC steps, estimates the acceptance rate for every tried molecular dynamics discretization and
+        fits a model to try and predict the number of molecular dynamics steps required to achieve the target acceptance rate.
+
+        Parameters
+        ----------
+        target_acceptance : float
+            The desired target acceptance rate.
+        continuum : int
+            The number of steps in the continuum for fitting the acceptance probabilities.
+        maxfev : int
+            The maximum number of function evaluations for the curve fitting optimization.
+
+        Returns
+        -------
+        int
+            The predicted number of molecular dynamics steps to achieve the target acceptance rate.
+
+        .. note::
+
+            This method uses a bootstrap estimator to estimate acceptance probabilities,
+            which is a gross simplification since we know the acceptance probabilities are in [0,1].
+
+            It uses the gamma distribution to model the estimated acceptance rates.
+        '''
+        # Based on
+        #
+        # Krieg, Luu, Ostmeyer, Papaphilippou, Urbach
+        #     Comput.Phys.Commun. 236 (2019) 15-25 10.1016/j.cpc.2018.10.008 1804.07195
+        # Accelerating Hybrid Monte Carlo simulations of the Hubbard model on the hexagonal lattice
+
+        points = deque()
+
+        for (md_steps, rows) in self.measurements.groupby('md_steps'):
+            # A simple bootstrap estimator for a very naive uncertainty on the acceptance probability:
+            acc=np.concatenate(rows['acc'].values)
+            boot=acc[torch.randint(0, len(acc), (self._bootstraps, len(acc)))].mean(axis=1)
+            points.append(pd.Series({'md_steps': md_steps, '<acc>': boot.mean(), 'd<acc>': boot.std()}))
+
+        self.summary = pd.DataFrame(points)
+
+        if len(points) < 3:
+            # Add fictitious points to stabilize the fit when there have been too few iterations.
+            points.append(pd.Series({'md_steps': 0, '<acc>': 0., 'd<acc>': 0.01}))
+            points.append(pd.Series({'md_steps': continuum, '<acc>': 1., 'd<acc>': 0.01}))
+
+        points = pd.DataFrame(points)
+
+        most_recent = points[points['md_steps'] == self.md_steps].iloc[0]
+
+        self.measurements.iloc[-1]['<acc>'] = most_recent['<acc>']
+        self.measurements.iloc[-1]['d<acc>'] = most_recent['d<acc>']
+
+        # The above reference fits estimated acceptance rates to the CDF of a skew normal distribution, which is currently not available in torch.
+        # I searched for other similar-looking CDFs and opted for the gamma distribution
+        #     https://pytorch.org/docs/stable/distributions.html#gamma
+        # but remarkably, even though https://github.com/pytorch/pytorch/issues/41637 is closed the distribution still does not have a cdf method!
+        # However, a comment there suggested trying torch.special.{gammainc,gammaincc}.
+        # That function is not differentiable, at least in the distribution parameters.  So it is not fittable using iterative torch back-prop methods.
+        # Annoying.
+        #   [
+        #       Actually, the issue has been reopened and is available in torch 2.0.0.
+        #       However, it is still not differentiable wrt the non-scale parameters, as this involves MeijerG
+        #   ]
+        #
+        # Instead, I fell back on to scipy for fitting.  Oh well!
+        # TODO: keep an eye on the availability of differentiable / fittable SkewNormal or Gamma distributions.
+
+        # a, loc, beta
+        previous = self._fit if self._fit is not None else np.array((1.0, 0.0, 1.0))
+
+        # We relax the bootstrap errors if they're too small for fit flexibility.
+        points['d<acc>'] = points['d<acc>'].clip(lower=uncertainty_floor)
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=scipy.optimize.OptimizeWarning)
+            # If the fit errors we want to know, but often the fitter cannot determine the covariance, warning
+            # OptimizeWarning: Covariance of the parameters could not be estimated
+            # which we do not care about.
+            self._fit, cov = scipy.optimize.curve_fit(
+                scipy.stats.gamma.cdf,
+                points['md_steps'].values, points['<acc>'].values,
+                p0=previous,
+                sigma=points['d<acc>'].values,
+                maxfev=maxfev,
+            )
+
+        self.measurements['fit'].iloc[-1] = self._fit
+        target = scipy.stats.gamma.ppf(target_acceptance, *self._fit)
+        self.measurements['prediction'].iloc[-1] = target
+        logger.info(f'The best estimate is that for {target_acceptance=} we should target {target} molecular dynamics steps.')
+
+        return max(self.md_steps_min, int(np.ceil(target)))
 
