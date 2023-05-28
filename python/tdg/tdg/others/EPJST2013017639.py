5ae04459490d985326044874edf438dda8e5ef14
Evan Berkowitz <git@evanberkowitz.com>
2023-05-28 12:47:52 +0200

    Add energy data to EPJST2013017639

--- python/tdg/tdg/others/EPJST2013017639.py
+++ python/tdg/tdg/others/EPJST2013017639.py
@@ -1,4 +1,5 @@
 import torch
+import tdg.conventions
 
 from tdg.others.citation import Citation
 citation = Citation(
@@ -13,6 +14,90 @@ citation = Citation(
     year={2013},
     publisher={Springer}
 }''')
+
+def figure_2():
+    r'''
+    Figure 2 provides the normalized, mean-field subtracted energy normalized directly but as a function of their :math:`g=-1/2 \log(k_F b)` (just after (2)) where :math:`b` sets the binding energy as :math:`-1/mb^2` (just below Figure 1).
+
+    Therefore, :math:`b` is the scattering length in our convention.  However, :math:`g=\alpha/2`.
+
+    Returns THREE tensors, data representing their green squares, blue triangles, and gray circles, respectively.
+    '''
+
+    citation()
+
+    # All are (alpha, (E_ - E_MF)/E_FG)
+    greenSquares = torch.tensor([
+        [ +0.40, +0.00 ],
+        [ +0.36, +0.02 ],
+        [ +0.32, +0.03 ],
+        [ +0.27, +0.03 ],
+        [ +0.23, +0.03 ],
+        [ +0.19, +0.02 ],
+        [ +0.14, +0.02 ],
+        [ +0.09, +0.01 ],
+        [ +0.05, +0.00 ],
+        ]).T
+
+    blueTriangles = torch.tensor([
+        [ +0.36, -0.01 ],
+        [ +0.32, -0.00 ],
+        [ +0.28, -0.00 ],
+        [ +0.23, +0.00 ],
+        [ +0.19, +0.00 ],
+        [ +0.14, +0.00 ],
+        [ +0.09, +0.00 ],
+        [ +0.05, +0.00 ],
+        ]).T
+
+    grayCircles = torch.tensor([
+        [ -0.37, +0.11 ],
+        [ -0.32, +0.08 ],
+        [ -0.26, +0.05 ],
+        [ -0.20, +0.03 ],
+        [ -0.15, +0.01 ],
+        [ -0.10, +0.00 ],
+        [ -0.07, +0.00 ],
+        [ -0.04, +0.00 ],
+        [ -0.02, +0.00 ],
+        ]).T
+
+    return greenSquares, blueTriangles, grayCircles
+
+def conventional_figure_2():
+    r'''
+    Rather than :math:`g`, gives data as a function of :math:`\alpha`.
+    '''
+    green, blue, gray = figure_2()
+
+    green[0] = 2*green[0]
+    blue [0] = 2*blue [0]
+    gray [0] = 2*gray [0]
+
+    return green, blue, gray
+
+def energy_comparison(ax, **kwargs):
+
+    green, blue, gray = conventional_figure_2()
+
+    ax.plot(
+            green[0],
+            green[1],
+            color='green', marker='s', linestyle='none',
+            label="SW UB-VMC [Bertaina (2013)]"
+            )
+    ax.plot(
+            blue [0],
+            blue [1],
+            color='blue', marker='v', linestyle='none',
+            label='HD JS-DMC [Bertaina (2013)]'
+            )
+    ax.plot(
+            gray [0],
+            gray [1],
+            color='gray', marker='o', linestyle='none',
+            label='SW JS-DMC [Bertaina (2013)]'
+            )
 
 def contact_by_kF4():
     r'''
