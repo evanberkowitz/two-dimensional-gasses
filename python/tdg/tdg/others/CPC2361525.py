25b20ced28fd6c4f9733b5cf39a218a141411151
Evan Berkowitz <git@evanberkowitz.com>
2023-06-20 15:29:06 +0200

    First citation() beyond data / formulae

---
+++ python/tdg/tdg/others/CPC2361525.py
@@ -0,0 +1,9 @@
+
+import torch
+import tdg.conventions
+
+from tdg.others.citation import Citation
+citation = Citation(
+    'Krieg et al., Comput. Phys. Commun 236:15-25 (2019)',
+    'Krieg:2018pqh'
+    )
